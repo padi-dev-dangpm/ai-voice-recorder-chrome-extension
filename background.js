699d24f57f8d26378af54ce6d673ab09c64e5a25
ledanghuy1811 <huygamo567@gmail.com>
2023-02-05 21:30:17 +0700

    add context menus

--- background.js
+++ background.js
@@ -1,14 +1,45 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-	chrome.runtime.onMessage.addListener((requester, sender, response) => {
-		if (requester.type == "RECORD") {
-			chrome.tabs.sendMessage(tabId, {
-				type: "NEW",
-			});
-		}
-		if (requester.type == "STOP") {
-			chrome.tabs.sendMessage(tabId, {
-				type: "UPLOAD",
-			});
-		}
-	});
+  chrome.runtime.onMessage.addListener((requester, sender, response) => {
+    if (requester.type == "RECORD") {
+      chrome.tabs.sendMessage(tabId, {
+        type: "NEW",
+      });
+    }
+    if (requester.type == "STOP") {
+      chrome.tabs.sendMessage(tabId, {
+        type: "UPLOAD",
+      });
+    }
+  });
 });
+
+// create context menus
+chrome.contextMenus.create({
+  id: "1",
+  title: "Click here to record",
+  contexts: ["page"],
+});
+
+chrome.contextMenus.onClicked.addListener(function () {
+  // baseURL = "https://www.facebook.com/";
+  // chrome.tabs.create({ url: baseURL });
+  //   console.log(chrome.runtime.getURL("popup.html"));
+
+  chrome.tabs.create(
+    {
+      url: chrome.runtime.getURL("popup.html"),
+      active: false,
+    },
+    function (tab) {
+      // After the tab has been created, open a window to inject the tab
+      chrome.windows.create({
+        tabId: tab.id,
+        type: "popup",
+        focused: true,
+        height: 320,
+        width: 320,
+      });
+    }
+  );
+});
+
